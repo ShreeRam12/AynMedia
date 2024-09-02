cd456a68da77e4725b351567d86ed27fc1223d05
ShreeRam12 <shreeramshree109@gmail.com>
2024-09-02 12:35:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      translate: {
+        // 'parallax': 'translate(-50%, calc(-50% + var(--yPos, 0px)))',
+      },
+    },
+  },
+  plugins: [],
+}
