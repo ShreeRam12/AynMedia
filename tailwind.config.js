52d548b268c1f63bee113adf14f03fe8bca3f960
ShreeRam12 <shreeramshree109@gmail.com>
2024-09-03 18:10:09 +0530

    Animations for Aynmeidia website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   theme: {
     extend: {
-      translate: {
-        // 'parallax': 'translate(-50%, calc(-50% + var(--yPos, 0px)))',
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 1s ease-in-out forwards',
       },
     },
   },
